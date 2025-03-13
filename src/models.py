a9d572f303b6042e457b4b9cfcb8f52b27a476aa
Isaac Leegit config --global user.name Isaac <shun.s.lee@kcl.ac.uk>
2025-03-13 14:55:26 +0000

    - train models to determine model accuracy

--- src/models.py
+++ src/models.py
@@ -1,1 +1,22 @@
 # Code for building, training, and evaluating your model
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+def train_models(X_train, y_train):
+    """"
+    train a Linear Regression model on historical stock data "
+    """
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    return model 
+
+def evaluate_model (model, X_test, y_test):
+    """"
+    Evaluate model performance using mean squared error"
+    """
+
+    predictions = model.predict(X_test)
+    mse = mean_squared_error(y_test, predictions)
+    return mse, predictions
+
+    
